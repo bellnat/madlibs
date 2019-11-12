5cbaa296cdb840d9c031e3ebff7d225e1c67bea4
bellnat <53783272+bellnat@users.noreply.github.com>
2019-11-12 13:40:29 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let adjective = prompt("Type an adjective");
+let verb = prompt("Type a verb ");
+let verb2 = prompt("Type another verb");
+let sentence = "There once was a " + adjective + " dragon who had a strong liking to " + verb + " and " + verb2 + ".";
+
+document.write(sentence);
